32efdcc9df733b2cfa70eaa5eb892937afa17e40
Sravan2j <sravan2j@gmail.com>
2013-12-15 05:17:59 -0800

    Cleaning Android layout code: Phase 3. Modified HomeActivity.java

--- Android/src/edu/sjsu/cinequest/HomeActivity.java
+++ Android/src/edu/sjsu/cinequest/HomeActivity.java
@@ -11,23 +11,18 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import edu.sjsu.cinequest.comm.Callback;
 import edu.sjsu.cinequest.comm.ImageManager;
 import edu.sjsu.cinequest.comm.Platform;
 import edu.sjsu.cinequest.comm.QueryManager;
-import edu.sjsu.cinequest.comm.cinequestitem.MobileItem;
 import edu.sjsu.cinequest.comm.cinequestitem.News;
 import edu.sjsu.cinequest.comm.cinequestitem.NewsFeed;
 import edu.sjsu.cinequest.comm.cinequestitem.User;
@@ -44,14 +39,10 @@ public class HomeActivity extends Activity {
 	private ListView list;
 	private String target;
 	ImageView title_image;
-	String lastupdated="";
 
 	private static QueryManager queryManager;
 	private static ImageManager imageManager;
-	private static User user;
 	LazyAdapter adapter;
-
-	private static String calendarName="Cinequest Calendar";
 
 	/** Called when the activity is first created. */
 	@Override
@@ -70,96 +61,14 @@ public class HomeActivity extends Activity {
 			// We tried...
 		}
 
-		/*String[] proj = new String[]{"_id", "calendar_displayName"};
-		String calSelection = "(calendar_displayName= ?) "; 				
-		String[] calSelectionArgs = new String[] {calendarName}; 
-		Uri event = Uri.parse("content://com.android.calendar/calendars");        
-
-		Cursor l_managedCursor = getContentResolver().query(event, proj, calSelection, calSelectionArgs, null );
-		if (l_managedCursor.getCount()<=0) {                                                    
-
-			ContentValues l_event = new ContentValues();
-			l_event.put("account_name", "Cinequest");
-			l_event.put("account_type", "LOCAL");                  
-			l_event.put("name", calendarName);
-			//l_event.put("displayName", "Cinequest Calendar");
-			//l_event.put("color", 0xffff0000);
-			//l_event.put("access_level",  700);
-			//l_event.put("timezone", TimeZone.getDefault().getID());
-			l_event.put("calendar_displayName", calendarName);
-			l_event.put("calendar_color", 0xffff0000);
-			l_event.put("calendar_access_level",  700);
-			l_event.put("calendar_timezone", TimeZone.getDefault().getID());
-			l_event.put("ownerAccount", "owner");
-			l_event.put("visible", 1);
-			Uri.Builder builder = event.buildUpon();
-			builder.appendQueryParameter("account_name", "Cinequest");
-			builder.appendQueryParameter("account_type", "LOCAL");                
-			builder.appendQueryParameter( "caller_is_syncadapter", "true");
-			Uri uri = getContentResolver().insert(builder.build(), l_event); 
-			l_managedCursor.close();
-			l_managedCursor=null;
-		}
-		 */
-		//Platform.setInstance(new AndroidPlatform(getApplicationContext()));
-		//queryManager = new QueryManager();
-		//queryManager = (QueryManager) getIntent().getSerializableExtra("queryManagerObj");
-		//queryManager = SplashScreenActivity.getQueryManager();
 		if (queryManager == null)
 		{
 			queryManager = new QueryManager();			
 		}
 		imageManager = new ImageManager();
-		//user = new User();
 
-
-		/*queryManager.getFestivalDates(new ProgressMonitorCallback(this) {
-			public void invoke(Object result)
-			{
-				super.invoke(result);
-				DateUtils.setFestivalDates((String[]) result);            
-			}
-		}); */       
-
-		/*title_image = (ImageView) this.findViewById(R.id.homescreen_title_image);
-		title_image.setImageDrawable(getResources().getDrawable(R.drawable.creative));
-*/
 		list = (ListView)this.findViewById(R.id.home_newslist);
-		/*list.setAdapter(new SeparatedListAdapter(this));
-
-		list.setOnItemClickListener( new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, 
-					int position, long id) {
-				MobileItem item = (MobileItem) list.getItemAtPosition(position);
-				Intent intent = new Intent();
-				intent.setClass(HomeActivity.this, FilmDetail.class);
-				intent.putExtra("target", item);
-				startActivity(intent);
-			}
-		});
-		 */
-		/*		Button festivalButton = (Button) findViewById(R.id.goto_festival_button);
-		festivalButton.setOnClickListener(new OnClickListener() {			
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(HomeActivity.this, MainTab.class);
-				i.putExtra("open_tab", MainTab.FILMS_TAB);
-				startActivity(i);
-			}
-		});
-
-		Button dvdButton = (Button) findViewById(R.id.goto_dvd_button);
-		dvdButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(HomeActivity.this, MainTab.class);
-				i.putExtra("open_tab", MainTab.DVDS_TAB);
-				startActivityForResult(i, 0);
-			}
-		});              
-		 */	}
+	}
 
 	/**
 	 * Called when activity resumes
@@ -170,9 +79,6 @@ public class HomeActivity extends Activity {
 		queryManager.getSpecialScreen("ihome", new Callback(){
 			@Override
 			public void invoke(Object result) {
-
-				//populateNewsEventsList((Vector<Section>) result);
-
 				populateNewsEventsList((NewsFeed) result);
 				// TODO: Why doesn't this work???
 				if (!((NewsFeed) result).getLastUpdated().equalsIgnoreCase(queryManager.getlastUpdated()))
@@ -186,7 +92,6 @@ public class HomeActivity extends Activity {
 	}
 
 	protected void onStop(){
-		//user.persistSchedule();
 		imageManager.close();
 		Platform.getInstance().close();
 		super.onStop();
@@ -196,16 +101,9 @@ public class HomeActivity extends Activity {
 	 * Display the header image and schedule to the user with 
 	 * section-title being separator-header.
 	 */
-	//private void populateNewsEventsList(Vector<Section> newsSections)
+	
 	private void populateNewsEventsList(NewsFeed newsSections)
-	{		
-		/*if (lastupdated!=newsSections.getLastUpdated() || lastupdated=="")
-		{
-			lastupdated=newsSections.getLastUpdated();
-			ImageLoader imageLoader = new ImageLoader(this);
-			imageLoader.clearCache();			
-		}*/
-		//title_image.setVisibility(View.GONE);
+	{				
 		final List<News> news=newsSections.getNewsList();
 		ArrayList<String> imgURL = new ArrayList<String>();
 		for (int i=0;i<news.size();i++)
@@ -215,50 +113,6 @@ public class HomeActivity extends Activity {
 		}
 		String[] imageURL=imgURL.toArray(new String[imgURL.size()]);
 
-		//if there is no news to display, return
-		/*if (newsSections.size() == 0) {
-			//Clear the items of previous list being displayed (if any)
-			list.setAdapter(new SeparatedListAdapter(this));
-			return;
-		}
-
-		// create our list and custom adapter  
-		SeparatedListAdapter separatedListAdapter = new SeparatedListAdapter(this);
-
-		for(int i = 0; i < newsSections.size(); i++) {
-			Section s = newsSections.get(i);
-			String sectionTitle = s.getTitle();
-			Vector items = s.getItems();
-
-			ArrayList<MobileItem> newsEvents = new ArrayList<MobileItem>();
-
-			if (i == 0) {
-				if (items.size() > 0) {
-					String imageURL = ((MobileItem) items.get(0)).getImageURL();
-					getImageManager().getImage(imageURL, new Callback() {
-						@Override public void invoke(Object result) {
-							title_image.setImageBitmap((Bitmap) result);	        		
-						}
-						@Override public void starting() {}
-						@Override public void failure(Throwable t) {
-							Platform.getInstance().log(t); 			        		
-						}
-					}, null, false);		
-				}
-			}
-			else {     		
-				for(int j = 0; j < items.size(); j++) {
-					newsEvents.add((MobileItem) items.get(j));     			
-				}
-				separatedListAdapter.addSection(sectionTitle, new MobileItemAdapter(this, R.layout.home_event_row, newsEvents));
-			}
-		}
-
-		list.setAdapter(separatedListAdapter);
-
-		 */	
-		/*SeparatedListAdapter adapter = new SeparatedListAdapter(this);
-		adapter.addSection("News",new LazyAdapter(this, imageURL, news));*/
 		adapter=new LazyAdapter(this, imageURL, news);
 		list.setAdapter(adapter);		
 		list.setOnItemClickListener(new OnItemClickListener() {
@@ -267,9 +121,6 @@ public class HomeActivity extends Activity {
 			public void onItemClick(AdapterView<?> parent, View view, int position,
 					long id) {
 
-				/*Object result = getListview().getItemAtPosition( position );
-				Log.i("tabactivity",result.getClass().toString());
-				launchFilmDetail(result);				*/
 				final Intent intent;
 				if (news.get(position).getInfoLink().trim().toLowerCase().startsWith("http"))
 				{
@@ -316,14 +167,6 @@ public class HomeActivity extends Activity {
 	}
 
 	/**
-	 * Get the User
-	 * @return user
-	 */
-	public static User getUser() {
-		return user;
-	}
-
-	/**
 	 * Create a menu to be displayed when user hits Menu key on device
 	 */
 	@Override
@@ -347,42 +190,5 @@ public class HomeActivity extends Activity {
 			return super.onOptionsItemSelected(item);
 		}
 
-	}
-
-	/**
-	 * Custom List-Adapter to show the MobileItems in news event list 
-	 */
-	private class MobileItemAdapter extends ArrayAdapter<MobileItem>{
-
-		private ArrayList<MobileItem> itemsList;
-		private int view_resource_id;	//id of the list's row's layout xml
-
-		public MobileItemAdapter(Context context, int textViewResourceId, ArrayList<MobileItem> list) {
-			super(context, textViewResourceId, list);
-			this.itemsList = list;
-			this.view_resource_id = textViewResourceId;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View v = convertView;
-
-			if (v == null) {
-				LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				v = vi.inflate(view_resource_id, null);
-			}
-			MobileItem result = itemsList.get(position);
-
-			if (result != null) {
-				//get text from list, and fill it into the row		
-				TextView title = (TextView) v.findViewById(R.id.news_event_title);                        
-
-				//Set title text
-				if (title != null)
-					title.setText(result.getTitle());     
-			}
-
-			return v;
-		}
 	}
 }
