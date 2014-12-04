92cfd7246344d4f1a43f5b2f0c5a4c14230f7f8b
farjahan <farjahan@mirtanvir.com>
2014-12-03 18:43:40 -0800

    Update/change current images to same size grey icons,Update Icons for non-active and active Buttons states, and Swap schedule tab and My Cinequest tab Icons

--- Android/src/edu/sjsu/cinequest/MainTab.java
+++ Android/src/edu/sjsu/cinequest/MainTab.java
@@ -1,16 +1,17 @@
 package edu.sjsu.cinequest;
 
-import android.graphics.Color;
 import android.app.TabActivity;
-import android.widget.TabHost.OnTabChangeListener;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+
 /**
- * Main tab is a tab that displays contents of News,Films, Events, Forums, Schedule
- * to the consumer. For easy navigation and information lookup.
- * This tab is th head tab from which consumer can navigate all necessary film related
- * information.
+ * Main tab is a tab that displays contents of News,Films, Events, Forums,
+ * Schedule to the consumer. For easy navigation and information lookup. This
+ * tab is th head tab from which consumer can navigate all necessary film
+ * related information.
  */
 public class MainTab extends TabActivity {
 
@@ -25,49 +26,66 @@ public class MainTab extends TabActivity {
 		Intent intent;
 
 		intent = new Intent().setClass(this, HomeActivity.class);
-		spec = tabHost.newTabSpec("news").setIndicator("News",getResources().getDrawable(R.drawable.news)).setContent(intent);
+		spec = tabHost
+				.newTabSpec("news")
+				.setIndicator("News",
+						getResources().getDrawable(R.drawable.ic_tab_news))
+				.setContent(intent);
 		tabHost.addTab(spec);
 
 		// Create the intent associated with the activity
 		intent = new Intent().setClass(this, IndexActivity.class);
 		intent.putExtra("tab", "index");
 		// Create a new TabSpec with a name, an icon and intent
-		spec = tabHost.newTabSpec("index").setIndicator("Index",getResources().getDrawable(R.drawable.film_icon)).setContent(intent);
+		spec = tabHost
+				.newTabSpec("index")
+				.setIndicator("Index",
+						getResources().getDrawable(R.drawable.ic_tab_film))
+				.setContent(intent);
 		// Add it to the tab
 		tabHost.addTab(spec);
 
 		intent = new Intent().setClass(this, FilmsActivity1.class);
 		intent.putExtra("tab", "schedule");
-		spec = tabHost.newTabSpec("schedule").setIndicator("Schedule",getResources().getDrawable(R.drawable.events_icon)).setContent(intent);
+		spec = tabHost
+				.newTabSpec("schedule")
+				.setIndicator("Schedule",
+						getResources().getDrawable(R.drawable.ic_tab_schedule))
+				.setContent(intent);
 		tabHost.addTab(spec);
 
-		intent = new Intent().setClass(this, ScheduleActivity.class);        
-		spec = tabHost.newTabSpec("My Cinequest").setIndicator("My Cinequest",getResources().getDrawable(R.drawable.schedule_icon)).setContent(intent);
+		intent = new Intent().setClass(this, ScheduleActivity.class);
+		spec = tabHost
+				.newTabSpec("My Cinequest")
+				.setIndicator("My Cinequest",
+						getResources().getDrawable(R.drawable.ic_tab_event))
+				.setContent(intent);
 		tabHost.addTab(spec);
 
 		// Default tab is the first tab.
 		int tab = getIntent().getIntExtra("open_tab", 0);
 		tabHost.setCurrentTab(tab);
-	
-		tabHost.setOnTabChangedListener(new OnTabChangeListener(){
+
+		tabHost.setOnTabChangedListener(new OnTabChangeListener() {
 
 			@Override
 			public void onTabChanged(String arg0) {
 				// TODO Auto-generated method stub
-				setTabColor(tabHost); 
+				setTabColor(tabHost);
 			}
-			
+
 		});
 		setTabColor(tabHost);
-    }
-    
-    //Set tab color
-    public void setTabColor(TabHost tabHost){
-    	for(int i=0; i < tabHost.getTabWidget().getChildCount(); i++)
-        {
-            tabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.BLACK); //inactive tabs
-        }
-    	tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab()).setBackgroundColor(Color.RED); //selected, active tabs
-    }
-    
+	}
+
+	// Set tab color
+	public void setTabColor(TabHost tabHost) {
+		for (int i = 0; i < tabHost.getTabWidget().getChildCount(); i++) {
+			tabHost.getTabWidget().getChildAt(i)
+					.setBackgroundColor(Color.BLACK); // inactive tabs
+		}
+		tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab())
+				.setBackgroundColor(Color.RED); // selected, active tabs
+	}
+
 }
