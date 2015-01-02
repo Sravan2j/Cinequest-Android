aca520d36d80de293e591953e81e1f672db4c400
ofek86 <dimabeofek@gmail.com>
2015-01-02 01:15:57 -0800

    Updated FilmsActivity1.java to anchor date when user scrolls through items

--- Android/src/edu/sjsu/cinequest/ScheduleActivity.java
+++ Android/src/edu/sjsu/cinequest/ScheduleActivity.java
@@ -1,5 +1,6 @@
 package edu.sjsu.cinequest;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -7,10 +8,10 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
-
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.ContentUris;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Build;
@@ -20,6 +21,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -29,7 +31,7 @@ import android.widget.TextView;
  * The Schedule Tab of the app 
  * 
  * @author Sravankumar Reddy Javaji
- *
+ * @author Dmitri Dimov
  */
 
 class EventData {
@@ -85,35 +87,44 @@ class EventData {
 		}
 	}
 }
+
 /**
  * Any film related events are going to be displayed with this class.
  * Allows consumer to view any film related information with ScheduleActivity class.
  */
-public class ScheduleActivity extends Activity {
+@SuppressLint("SimpleDateFormat")
+public class ScheduleActivity extends CinequestActivity {
 	ListView listView;	
 	SimpleDateFormat sdf;
 	private DateUtils du;
 	String fStartTime="";
 	String fEndTime="";
 	boolean is24HourFormat=false;
-	//Configuration config;
-	//DateFormat dtformat;	
 	private static String calendarName="Cinequest Calendar";
 	private static String m_selectedCalendarId = "Cinequest Calendar";
 	private static final String DATE_TIME_FORMAT = "MMM dd, yyyy'T'HH:mm";
 	private List<EventData> events = new ArrayList<EventData>();
+	private TextView nothingToday;	//Shows an empty list when the list is empty
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.schedulelayout);
 		sdf = new SimpleDateFormat(DATE_TIME_FORMAT);
 		du = new DateUtils();
-		//config = getApplicationContext().getResources().getConfiguration();
-		//dtformat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, config.locale);		
+		nothingToday = (TextView) this.findViewById(R.id.msg_for_empty_schedyle);
+		listView = (ListView) findViewById(R.id.schedule_listview);
+		registerForContextMenu(listView);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();  // Always call the superclass method first
+		is24HourFormat=android.text.format.DateFormat.is24HourFormat(this);
+		events.clear();
+		populateSchedule();
 	}
 
 	public void populateSchedule()
 	{
-
 		String[] proj = new String[]{"_id", "calendar_displayName"};
 		String calSelection = 
 				"(calendar_displayName= ?) ";
@@ -183,7 +194,7 @@ public class ScheduleActivity extends Activity {
 				StartDateInMillis = Long.parseLong(l_managedCursor.getString(l_colBegin));
 				EventData edata= new EventData(e_id, l_title, l_begin, l_end, StartDateInMillis);
 				events.add(edata);
-			} 
+			}
 			while (l_managedCursor.moveToNext());
 		}
 
@@ -191,81 +202,136 @@ public class ScheduleActivity extends Activity {
 		//Collections.sort(events, new EventData.CompName());
 
 		Collections.sort(events, new EventData.CompDate());
-
-		ArrayAdapter<EventData> adapter = new ArrayAdapter<EventData>(
-				this.getApplicationContext(), R.layout.eventlistview, events) {
+		SeparatedListIndexedAdapter separatedSchedule = new SeparatedListIndexedAdapter(this){
 			@Override
-			public View getView(final int position, View v, ViewGroup parent) {
+			public View getView(int position, View v, ViewGroup parent){
+				int sectionnum = 0;
+				for(Object section : this.sections.keySet()) {  
+					Adapter adapter = sections.get(section);  
+					int size = adapter.getCount() + 1;
 
-				LayoutInflater inflater = LayoutInflater.from(getContext());
-				final EventData q = getItem(position);                                
-				if (v == null) v = inflater.inflate(R.layout.eventlistview, null);                                
-				TextView textView = (TextView) v.findViewById(R.id.eventName);
-				textView.setText(q.getName());
-
-				String sStr[]=q.getStime().split("T");
-				String eStr[]=q.getEtime().split("T");
-				TextView textView1 = (TextView) v.findViewById(R.id.startTime);
-				//if start date and end date of event won't match, then we will be displaying both the dates.
-				//Else we will display only date once, followed by time.
-				if (sStr[0].equalsIgnoreCase(eStr[0]))
-				{
-					if(!is24HourFormat)
+					// check if position inside this section  
+					if(position == 0 && headers.getCount() > 0) return headers.getView(sectionnum, v, parent);
+					else if(position < size && size > 1)
 					{
- 
-					fStartTime=du.formatTime(sStr[1]);
-					fEndTime=du.formatTime(eStr[1]);
-					}
-					else
-					{
-						fStartTime=sStr[1];
-						fEndTime=eStr[1];
-					}
-					textView1.setText(sStr[0]+"  Time: "+fStartTime+" - "+fEndTime);
-				}
-				else
-				{
-					textView1.setText(sStr[0]+" "+sStr[1]+" - "+eStr[0]+" "+eStr[1]);
-				}
-				textView1.setTypeface(null, Typeface.ITALIC);
+						LayoutInflater inflater = LayoutInflater.from(getBaseContext());
+						final EventData q = (EventData) adapter.getItem(position - 1);
+						if (v == null) v = inflater.inflate(R.layout.eventlistview, null);                                
+						TextView textView = (TextView) v.findViewById(R.id.eventName);
+						textView.setText(q.getName());
 
-				Button button1 = (Button) v.findViewById(R.id.remove);
-				button1.setOnClickListener( new OnClickListener() {
-					@Override
-					public void onClick(View v) {
-						Uri eventUri;                    
-						if (Build.VERSION.SDK_INT >= 8) {
-							eventUri = Uri.parse("content://com.android.calendar/events");
-						} else {
-							//Calendar code for API level < 8, needs lot of testing. 
-							//May be some of the paramters (that we are populating above), have different naming conventions in different API Levels
-							eventUri = Uri.parse("content://calendar/events");
-						}                        
-						Uri deleteUri = ContentUris.withAppendedId(eventUri, q.getEId());
-						try{
-							int rows = getContentResolver().delete(deleteUri, null, null);
-							if (rows==1){
-								events.remove(position);
-								listView.invalidateViews();
+						String sStr[]=q.getStime().split("T");
+						String eStr[]=q.getEtime().split("T");
+
+						TextView textView1 = (TextView) v.findViewById(R.id.startTime);
+						//if start date and end date of event won't match, then we will be displaying both the dates.
+						//Else we will display only date once, followed by time.
+						if (sStr[0].equalsIgnoreCase(eStr[0]))
+						{
+							if(!is24HourFormat)
+							{
+								fStartTime=du.formatTime(sStr[1]);
+								fEndTime=du.formatTime(eStr[1]);
 							}
+							else
+							{
+								fStartTime=sStr[1];
+								fEndTime=eStr[1];
+							}
+							textView1.setText(sStr[0]+"  Time: "+fStartTime+" - "+fEndTime);
 						}
-						catch (Exception e){
-							Log.i("ScheduleActivity:populateSchedule","Error while removing Events from Calendar");
+						else
+						{
+							textView1.setText(sStr[0]+" "+sStr[1]+" - "+eStr[0]+" "+eStr[1]);
 						}
-
+						textView1.setTypeface(null, Typeface.ITALIC);
+						Button button1 = (Button) v.findViewById(R.id.remove);
+						button1.setOnClickListener( new OnClickListener() {
+							@Override
+							public void onClick(View v) {
+								Uri eventUri;                    
+								if (Build.VERSION.SDK_INT >= 8) {
+									eventUri = Uri.parse("content://com.android.calendar/events");
+								} else {
+									//Calendar code for API level < 8, needs lot of testing. 
+									//May be some of the parameters (that we are populating above), have different naming conventions in different API Levels
+									eventUri = Uri.parse("content://calendar/events");
+								}
+								String day = localizeHumanFormat(q.getStime());
+								Uri deleteUri = ContentUris.withAppendedId(eventUri, q.getEId());
+								try{
+									int rows = getContentResolver().delete(deleteUri, null, null);
+									if (rows==1){
+										//remove object from adapter
+										((ArrayAdapter<EventData>)sections.get(day)).remove(q);
+										if(sections.get(day).getCount() == 0)
+										{  //only if there is no more objects in adapter 
+											sections.remove(day);
+											headers.remove(day); //remove section
+										}
+										notifyDataSetChanged();
+									}
+								}
+								catch (Exception e){
+									Log.i("ScheduleActivity:populateSchedule","Error while removing Events from Calendar");
+								}
+							}
+						});
+						return v;
 					}
-				});
-				return v;                                
-			}                            
+					// otherwise jump into next section
+					position -= size;
+		            sectionnum++;
+				}
+				v = new View(getApplicationContext());
+				v.setBackgroundColor(Color.BLACK);
+				return v;
+			}
 		};
-		listView = (ListView) findViewById(R.id.schedule_listview);
-		listView.setAdapter(adapter);
 
-		listView.setItemsCanFocus(false);
+		for(EventData ev: events) //populates separated list indexed adapters
+		{
+			String day = localizeHumanFormat(ev.getStime());
+			if(separatedSchedule.haveSection(day))
+			{
+				((ArrayAdapter<EventData>)separatedSchedule.getAdapterForSection(day)).add(ev);
+			}
+			else
+			{
+				ArrayAdapter<EventData> temp = new ArrayAdapter<EventData>(this, R.layout.eventlistview);
+				temp.add(ev);
+				separatedSchedule.addSection(day, temp);
+			}
+		}
+		separatedSchedule.setAsAdapterFor(listView);
+
+		if(separatedSchedule.getCount() == 0){
+			listView.setVisibility(View.GONE);
+			nothingToday.setVisibility(View.VISIBLE);
+		}else{
+			listView.setVisibility(View.VISIBLE);
+			nothingToday.setVisibility(View.GONE);
+		}
+		listView.setFocusable(false);
 		l_managedCursor.close();
 		l_managedCursor=null;
 	}
 
+	@SuppressLint("SimpleDateFormat")
+	private String localizeHumanFormat(String inputDate)
+	{
+		SimpleDateFormat fmt = new SimpleDateFormat(DATE_TIME_FORMAT);
+		Date date = null;
+		try {
+			date = fmt.parse(inputDate);
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
+		String dateInHumForm = (String)android.text.format.DateFormat.format("EEEE, MMMM dd", date);
+		String[] temp = dateInHumForm.split(" ");
+		int dayOfMonth = Integer.parseInt(temp[2]);
+		return temp[0] + " " + temp[1] + " " + dayOfMonth;
+	}
 
 	@SuppressWarnings("deprecation")
 	public static String getDateTimeStr(int p_delay_min) {
@@ -283,14 +349,5 @@ public class ScheduleActivity extends Activity {
 	public String getDateTimeStr(String p_time_in_millis) {	
 		Date l_time = new Date(Long.parseLong(p_time_in_millis));
 		return sdf.format(l_time);
-		//return dtformat.format(l_time);    //to get the locale date
 	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();  // Always call the superclass method first
-		is24HourFormat=android.text.format.DateFormat.is24HourFormat(this);
-		events.clear();
-		populateSchedule();
-	}			
 }
--- Android/src/edu/sjsu/cinequest/SeparatedListAdapter.java
+++ Android/src/edu/sjsu/cinequest/SeparatedListAdapter.java
@@ -4,16 +4,12 @@ package edu.sjsu.cinequest;
 // http://jsharkey.org/blog/2008/08/18/separating-lists-with-headers-in-android-09/
 // just added the second link that has the same content as the first link, but it shows the image of separating lists with headers.
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.TreeMap;
 import java.util.Vector;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.View;
@@ -27,39 +23,60 @@ import edu.sjsu.cinequest.comm.cinequestitem.Schedule;
  * A separating list adapter with headers that can be used in listview.
  */
 public class SeparatedListAdapter extends BaseAdapter {
-    public Map<String,Adapter> sections = new LinkedHashMap<String,Adapter>();  
-    public ArrayAdapter<String> headers;
-    public Vector<Schedule> list;
-    public final static int TYPE_SECTION_HEADER = 0;  
-    public boolean SortKeys;
-    
-    public SeparatedListAdapter(Context context) {
-    	SortKeys = false;
-        headers = new ArrayAdapter<String>(context, R.layout.list_header);
-    }
-    
-    public void setList(Vector<Schedule> list)
-    {
-    	this.list = list;
-    }
+	public Map<String,Adapter> sections = new LinkedHashMap<String,Adapter>();  
+	public ArrayAdapter<String> headers;
+	public Vector<Schedule> list;
+	public final static int TYPE_SECTION_HEADER = 0;
+	public boolean SortKeys;
+
+	public SeparatedListAdapter(Context context) {
+		SortKeys = false;
+		headers = new ArrayAdapter<String>(context, R.layout.list_header);
+	}
+
+	public void setList(Vector<Schedule> list)
+	{
+		this.list = list;
+	}
 
 	public void addSection(String section, Adapter adapter) {  
-        this.headers.add(section);
-        this.sections.put(section, adapter);
-    }
-   
-    /*
-     * Get the number of items in the data set represented by this adapter.
-     * @return the number of all sections in listview.
-     */
-	public int getCount() {
-		// total together all sections, plus one for each section header  
-        int total = 0;  
-        for(Adapter adapter : this.sections.values())  
-            total += adapter.getCount() + 1;  
-        return total;  
+		addSectionName(section);
+		this.sections.put(section, adapter);
+	}
+
+	/**
+	 * Adds a section name to headers
+	 **/
+	public void addSectionName(String section)
+	{
+		if(!section.equals("") && headers.getPosition(section) == -1)
+			headers.add(section);
 	}
 	
+	/**
+	 * Removes a section from sections including headers
+	 * */
+	public void removeSection(String section)
+	{
+		if(section.contains(section))
+		{
+			sections.remove(section);
+			headers.remove(section); //remove section
+		}
+	}
+	
+	/*
+	 * Get the number of items in the data set represented by this adapter.
+	 * @return the number of all sections in listview.
+	 */
+	public int getCount() {
+		// total together all sections, plus one for each section header  
+		int total = 0;  
+		for(Adapter adapter : this.sections.values())  
+			total += adapter.getCount() + 1;  
+		return total;  
+	}
+
 	/*
 	 * Get the data item associated with the specified position in the data set
 	 */
@@ -71,87 +88,73 @@ public class SeparatedListAdapter extends BaseAdapter {
 		}
 		//else return from the sections Map
 		for(Object section : this.sections.keySet()) {
-            Adapter adapter = sections.get(section);  
-            
-            int size = adapter.getCount() + 1;  
-  
-            // check if position inside this section  
-            if(position  == 0) return section;
-            else if(position < size){return adapter.getItem(position - 1); }  
-  
-            // otherwise jump into next section  
-            position -= size;  
-        }  
+			Adapter adapter = sections.get(section);  
+
+			int size = adapter.getCount() + 1;  
+
+			// check if position inside this section  
+			if(position  == 0) return section;
+			else if(position < size){ return adapter.getItem(position - 1); }  
+
+			// otherwise jump into next section  
+			position -= size;  
+		}
 		return null;
-		
 	}
 
 	/*
 	 * Get the number of types of views that will be created by getView().
 	 */
 	public int getViewTypeCount() {  
-        // assume that headers count as one, then total all sections  
-        int total = 1;  
-        for(Adapter adapter : this.sections.values())  
-            total += adapter.getViewTypeCount();  
-        return total;  
-    }
-	
+		// assume that headers count as one, then total all sections  
+		int total = 1;  
+		for(Adapter adapter : this.sections.values())  
+			total += adapter.getViewTypeCount();  
+		return total;  
+	}
+
 	/*
 	 * Get the type of View that will be created by getView().
 	 */
 	public int getItemViewType(int position)
 	{
-        int type = 1;
-        for(Object section : this.sections.keySet()) {  
-            Adapter adapter = sections.get(section);  
-            int size = adapter.getCount() + 1;  
-  
-            // check if position inside this section  
-            if(position == 0) return TYPE_SECTION_HEADER;  
-            else if(position < size) return type + adapter.getItemViewType(position - 1);  
-  
-            // otherwise jump into next section  
-            position -= size;  
-            type += adapter.getViewTypeCount();  
-        }  
-        return -1;  
-    }
-  	
-  	/*
-  	 * Indicates whether all the items in this adapter are selectable.
-  	 */
-    public boolean areAllItemsSelectable() {  
-        return false;  
-    }  
-  	
-  	/*
-  	 * Indicates whether the item at the specified position is enabled.
-  	 */
-    public boolean isEnabled(int position) {  
-        return (getItemViewType(position) != TYPE_SECTION_HEADER);  
-    }  
-	
+		int type = 1;
+		for(Object section : this.sections.keySet()) {  
+			Adapter adapter = sections.get(section);  
+			int size = adapter.getCount() + 1;  
+
+			// check if position inside this section  
+			if(position == 0) return TYPE_SECTION_HEADER;  
+			else if(position < size) return type + adapter.getItemViewType(position - 1);  
+
+			// otherwise jump into next section  
+			position -= size;  
+			type += adapter.getViewTypeCount();  
+		}
+		return -1;  
+	}
+
+	/*
+	 * Indicates whether all the items in this adapter are selectable.
+	 */
+	public boolean areAllItemsSelectable() {  
+		return false;  
+	}  
+
+	/*
+	 * Indicates whether the item at the specified position is enabled.
+	 */
+	public boolean isEnabled(int position) {  
+		return (getItemViewType(position) != TYPE_SECTION_HEADER);  
+	}  
+
 	/*
 	 * Get the row id associated with the specified position in the list.
 	 */
 	public long getItemId(int position) {
 		return position; 
 	}
-	
-	/**
-	 * This function lets BaseAdapter know when to redraw itself if changes
-	 * are made to its contends, in this case, when elements are being removed.
-	 * */
-	public void updateSections(String section, EventData object) {
-        ArrayAdapter<EventData> temp = ((ArrayAdapter<EventData>)this.sections.get(section));
-        temp.remove(object);
-        this.sections.remove(section);
-        this.sections.put(section, temp);
-        this.headers.remove(section);
-        notifyDataSetChanged();
-    }
-	
+
 	/*
 	 * Get a view that displays the data at the specified position in the data set.
 	 */
@@ -202,17 +205,17 @@ public class SeparatedListAdapter extends BaseAdapter {
 			}
 		}
 		for(Object section : this.sections.keySet()) {  
-            Adapter adapter = sections.get(section);  
-            int size = adapter.getCount() + 1;
-  
-            // check if position inside this section  
-            if(position == 0) return headers.getView(sectionnum, convertView, parent);  
-            else if(position < size) return adapter.getView(position - 1, convertView, parent);  
-  
-            // otherwise jump into next section  
-            position -= size;
-            sectionnum++;
-        }
+			Adapter adapter = sections.get(section);  
+			int size = adapter.getCount() + 1;
+
+			// check if position inside this section  
+			if(position == 0) return headers.getView(sectionnum, convertView, parent);  
+			else if(position < size) return adapter.getView(position - 1, convertView, parent);  
+
+			// otherwise jump into next section  
+			position -= size;
+			sectionnum++;
+		}
 		return null;
 	}
 }
